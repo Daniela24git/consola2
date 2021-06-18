ab85c739eb0c412a1b7b1a874d97e95ed1765027
Daniela24git <danielatorresdanixx2014@hotmail.com>
2021-06-18 03:17:54 -0500

    datospersona

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Fist_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digita tu nombre:");
+            var nombre = Console.ReadLine();
+
+            Console.WriteLine("Digita tu apellido:");
+            var apellido = Console.ReadLine(); 
+            
+            Console.WriteLine("Ingresa tu direccion de domicilio:");
+            var dirección = Console.ReadLine();
+
+            Console.WriteLine("Ingresa tu estado civil:");
+            var EstadoCivil = Console.ReadLine();
+
+            Console.WriteLine("Digita tu edad:");
+            int edad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresa tu peso:");
+            int peso = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresa tu altura:");
+            double altura = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el lugar en que nacio:");
+            var LugarNac = Console.ReadLine();
+
+            Console.WriteLine("Ingresa tu pais:");
+            var pais = Console.ReadLine();
+
+            Console.WriteLine("Ingresa tu ciudad:");
+            var ciudad = Console.ReadLine();
+            DateTime fechaIngreso = DateTime.Now;
+
+            Persona nombreP;
+            nombreP = new Persona();
+
+            Console.WriteLine("================DATOS PERSONALES====================");
+            Console.WriteLine(nombreP.datosPersona(nombre, apellido, dirección));
+
+            Console.WriteLine(nombreP.datosNum(edad, peso, altura));
+
+            Console.WriteLine(nombreP.datosCiud(LugarNac, pais, ciudad));
+
+            Console.WriteLine("Fecha Actual: " + fechaIngreso);
+            Console.ReadKey();
+        }
+    }
+}
+
+    
+          
+
+
---
+++ clasepersona.cs
@@ -0,0 +1,17 @@
+﻿class Persona
+{
+
+    public string datosPersona(string nombre, string apellido, string dirección)
+    {
+        return "Nombres: " + nombre + " " + apellido + " " + dirección + ".";
+    }
+    public string datosNum(int edad, int peso, double altura)
+    {
+        return "Tienes: " + edad + " años\n Tu peso es:" + peso + " Kg.\n Tu estatura es: " + altura + "cm.";
+    }
+    public string datosCiud(string LugarNac, string pais, string ciudad)
+    {
+        return "Estado civil: " + LugarNac + "\n Pais: " + pais + "\n Ciudad: " + ciudad + ".";
+    }
+
+}
